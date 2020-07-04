a19103e49073c7a6a47bae2528fa0e77852c717e
Tibor Zombory <contact@elasticmind.design>
2020-07-04 20:57:27 +0200

    Add some extra content

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -73,7 +73,7 @@ export const App = () => {
               Sheetz
             </h1>
             <h2 className="subtitle">
-              Simple, persisted.
+              Simple, fast, persisted.
             </h2>
           </div>
         </div>
@@ -89,6 +89,44 @@ export const App = () => {
             </>
         }
       </main>
+      <section className="content px-6">
+        <h2>
+          Instructions
+        </h2>
+        <p>
+          Select a cell to edit its contents. Content can either be a number or a formula.
+          A formula must start with a '=' sign and it can only contain references to cells in range and plus signs.
+        </p>
+        <p>
+          Some valid formulas:
+        </p>
+        <ul>
+          <li>'=A2'</li>
+          <li>'=A2+B2+C2'</li>
+          <li>'=A2+A2+A2'</li>
+        </ul>
+        <p>
+          Some invalid formulas:
+        </p>
+        <ul>
+          <li>'A2'</li>
+          <li>'=K11'</li>
+          <li>'=A2*A3'</li>
+        </ul>
+        <p>
+          You can deselect a selected cell by clicking on the upper left corner of the table.
+        </p>
+        <p>
+          Feel free to edit as you wish, all your changes are persisted. :)
+        </p>
+      </section>
+      <footer className="footer">
+        <div className="content has-text-centered">
+          <p>
+            <strong>Sheetz</strong> by <a href="https://elasticmind.design">Tibor Zombory</a>, 2020.
+          </p>
+        </div>
+      </footer>
     </div >
   );
 }
